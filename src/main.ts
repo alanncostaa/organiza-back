90e2c48a5815a47ae6adb1bf904bc2de2126f2bf
Jose Alan da Costa <allann.costa.ac@gmail.com>
2025-07-20 20:31:04 -0300

    Atualizando Porta

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3333);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
