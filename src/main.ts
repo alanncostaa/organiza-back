b534139e637f7959575d9ea59b2ff163fa400b02
Jose Alan da Costa <allann.costa.ac@gmail.com>
2025-07-20 21:37:32 -0300

    Alteração CORS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: "*", 
+    origin: "['http://localhost:3000']", // libera apenas o frontend local
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
