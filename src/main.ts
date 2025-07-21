9b2332f54241a1cbc54f9229fa4b3fbd963578ea
Jose Alan da Costa <allann.costa.ac@gmail.com>
2025-07-20 21:40:29 -0300

    Alteração CORS

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: "['http://localhost:3000']", // libera apenas o frontend local
+    origin: "['http://localhost:3000','https://organiza-front.vercel.app/']",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
